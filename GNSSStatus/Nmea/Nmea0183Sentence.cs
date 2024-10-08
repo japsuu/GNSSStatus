a5d95c369ef9e92b220ec24885b80b66d690c4ba
Jasper <japsu.honkasalo@gmail.com>
2024-10-08 15:28:04 +0300

    Implement NMEA0183 primitives

---
+++ GNSSStatus/Nmea/Nmea0183Sentence.cs
@@ -0,0 +1,41 @@
+﻿namespace GNSSStatus.Nmea;
+
+public readonly struct Nmea0183Sentence
+{
+    public readonly Nmea0183SentenceType Type;
+    public readonly string Data;
+
+
+    public Nmea0183Sentence(string data)
+    {
+        if (!data.StartsWith('$'))
+            throw new ArgumentException("NMEA0183 sentences must start with a '$' character.");
+        
+        Type = ParseType(data.Substring(3, 3));
+        Data = data;
+    }
+    
+    
+    private static Nmea0183SentenceType ParseType(string type)
+    {
+        if (type.Length != 3)
+            throw new ArgumentException("NMEA0183 sentence types must be 3 characters long.");
+        
+        return type switch
+        {
+            "GBS" => Nmea0183SentenceType.GBS,
+            "GGA" => Nmea0183SentenceType.GGA,
+            "GLL" => Nmea0183SentenceType.GLL,
+            "GSA" => Nmea0183SentenceType.GSA,
+            "GST" => Nmea0183SentenceType.GST,
+            "GSV" => Nmea0183SentenceType.GSV,
+            "RMC" => Nmea0183SentenceType.RMC,
+            "VTG" => Nmea0183SentenceType.VTG,
+            "ZDA" => Nmea0183SentenceType.ZDA,
+            _ => Nmea0183SentenceType.UNKNOWN
+        };
+    }
+
+
+    public override string ToString() => $"NMEA0183 Sentence: {Type} - {Data}";
+}
---
+++ GNSSStatus/Nmea/Nmea0183SentenceType.cs
@@ -0,0 +1,54 @@
+﻿namespace GNSSStatus.Nmea;
+
+public enum Nmea0183SentenceType
+{
+    /// <summary>
+    /// Unknown sentence type.
+    /// </summary>
+    UNKNOWN,
+    
+    /// <summary>
+    /// Satellite Fault Detection.
+    /// </summary>
+    GBS,
+    
+    /// <summary>
+    /// Fix Data.
+    /// </summary>
+    GGA,
+    
+    /// <summary>
+    /// Geographic Position: Latitude/Longitude.
+    /// </summary>
+    GLL,
+    
+    /// <summary>
+    /// DOP and active satellites.
+    /// </summary>
+    GSA,
+    
+    /// <summary>
+    /// Pseudorange Noise Statistics.
+    /// </summary>
+    GST,
+    
+    /// <summary>
+    /// Satellites in view.
+    /// </summary>
+    GSV,
+    
+    /// <summary>
+    /// Recommended Minimum: position, velocity, time.
+    /// </summary>
+    RMC,
+    
+    /// <summary>
+    /// Track made good and Ground speed.
+    /// </summary>
+    VTG,
+    
+    /// <summary>
+    /// Time & Date - UTC, day, month, year and local time zone.
+    /// </summary>
+    ZDA,
+}
