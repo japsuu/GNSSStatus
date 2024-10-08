a5dc8fd48743651eed5bf25da1ddbc7c2818afed
Juha <e2000936@edu.vamk.fi>
2024-10-08 12:15:03 +0300

    Added Comments

--- GNSSStatus/CoordinateConverter.cs
+++ GNSSStatus/CoordinateConverter.cs
@@ -2,5 +2,7 @@
 
 public class CoordinateConverter
 {
-    
+    //Funktion tarkoituksena ottaa sisään Decimal Degrees koordinaatit GGA viestistä ja muuntaa ne haluttuun GK järjestelmään
+    //Funktio siis ottaa sisään N ja E koordinaatit sekä GK järjestelmän numeron.
+    //Palauttaa X,Y koordinaatit GK järjestelmässä.
 }
