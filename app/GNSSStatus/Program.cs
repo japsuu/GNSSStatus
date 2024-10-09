f09160d191ef6b6d77d1fc3b23c5d92b372d3348
Juha <e2000936@edu.vamk.fi>
2024-10-09 19:29:44 +0300

    No need convert int to int

--- app/GNSSStatus/Program.cs
+++ app/GNSSStatus/Program.cs
@@ -114,7 +114,7 @@ internal static class Program
             string directionLongitudi = parts[5];
             string quality = parts[6];
             
-            GKCoordinate gk = CoordinateConverter.ConvertToGk(latitudi, longitudi, directionLatitudi, directionLongitudi, Convert.ToInt32(ConfigManager.CurrentConfiguration.GkValue), altitude);
+            GKCoordinate gk = CoordinateConverter.ConvertToGk(latitudi, longitudi, directionLatitudi, directionLongitudi, ConfigManager.CurrentConfiguration.GkValue, altitude);
 
             Logger.LogInfo($"GK21 X: {gk.N.ToString("#.000")} Y: {gk.E.ToString("#.000")} N2000 Korkeus: {gk.Z.ToString("#.000")}");
             
