981860085950fafba7375d944dea5949045081f2
Jasper <japsu.honkasalo@gmail.com>
2024-10-10 11:22:52 +0300

    Adjust config property naming

--- app/GNSSStatus/Configuration/ConfigurationData.cs
+++ app/GNSSStatus/Configuration/ConfigurationData.cs
@@ -10,9 +10,9 @@ public class ConfigurationData
     public required int GkSystemNumber { get; set; }
     
     // Static rover antenna base point
-    public required double BaseX { get; set; }
-    public required double BaseY { get; set; }
-    public required double BaseZ { get; set; }
+    public required double BaseLocationX { get; set; }
+    public required double BaseLocationY { get; set; }
+    public required double BaseLocationZ { get; set; }
     
     // MQTT
     public required string MqttBrokerAddress { get; set; }
